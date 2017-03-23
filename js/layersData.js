dceda347f1dcc4dc7e7486b8d185ace2233c86ca
Davide <davide.caviglia@gisweb.it>
2017-03-23 15:11:14 +0100

    aggiornamento layers

--- js/layersData.js
+++ js/layersData.js
@@ -1,3 +1,5 @@
 var mapLayers = [
-{"name":"particelle_606","type":"WMS","maxZoom":18,"minZoom":8,"opacity":0.3,"url":"http://camogli.iolweb.it/cgi-bin/mapserv?map=/apps/GisClient-2/mapset/map/camogli_pubblica.map"}
+{"name":"particelle_606","type":"WMS","maxZoom":18,"minZoom":8,"opacity":0.5,"url":"http://camogli.iolweb.it/cgi-bin/mapserv?map=/apps/GisClient-2/mapset/map/camogli_pubblica.map"},
+{"name":"fabbricati_607","type":"WMS","maxZoom":18,"minZoom":8,"opacity":0.8,"url":"http://camogli.iolweb.it/cgi-bin/mapserv?map=/apps/GisClient-2/mapset/map/camogli_pubblica.map"}
 ]
+
